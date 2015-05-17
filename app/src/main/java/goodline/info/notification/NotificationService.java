733596017e0baf9ee564ca207a830a62249d4b05
Fesswood <Fesswood@rambler.ru>
2015-05-17 22:39:30 +0700

    added notification Service and Receiver

--- app/src/main/java/goodline/info/notification/NotificationService.java
+++ app/src/main/java/goodline/info/notification/NotificationService.java
@@ -1,16 +1,25 @@
 package goodline.info.notification;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.util.Log;
 
-import java.util.List;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import goodline.info.boardrider.BoardNews;
 import goodline.info.boardrider.BoardRiderFragment;
 import goodline.info.boardrider.NewsLoader;
+import goodline.info.boardrider.R;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -24,6 +33,10 @@ public class NotificationService extends IntentService {
     private NewsLoader mDataLoader;
     private static final String DEBUG_TAG = "NotificationService";
 
+    public static final String ACTION_NOTIFY = "com.androidhotel.simplenotification.SimpleIntentService.DATA_RETRIEVED";
+    public static final String PARAM_RECEIVE_NEWS = "receiveNews";
+    public static final String PARAM_NEWS_FLAG = "toBoardNewsFragment";
+
     public NotificationService() {
         super(DEBUG_TAG);
 
@@ -32,15 +45,17 @@ public class NotificationService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         mDataLoader=new NewsLoader("http://live.goodline.info/guest/page",getApplicationContext());
-        mNewsFromBD = intent.getParcelableExtra(BoardRiderFragment.SERVICE_STANDARD);
+        mNewsFromBD = intent.getParcelableExtra(BoardRiderFragment.NEWS_TO_COMPARE);
 
         try {
             boolean isFetchingResultSucces = mDataLoader.fetchLastNews();
             if(isFetchingResultSucces){
                 BoardNews boardNews = mDataLoader.getData().get(0);
                 int isFetchingNewsLater = mNewsFromBD.compareTo(boardNews);
+                isFetchingNewsLater=1;
                 if(isFetchingNewsLater==1){
                     Log.d(DEBUG_TAG, "News received!");
+                    sendNotif(boardNews);
                 }
             }
         } catch (ExecutionException e) {
@@ -48,6 +63,50 @@ public class NotificationService extends IntentService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    private void sendBroadcast(BoardNews receiveNews)
+    {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(NotificationService.ACTION_NOTIFY);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        broadcastIntent.putExtra(PARAM_RECEIVE_NEWS, receiveNews);
+        sendBroadcast(broadcastIntent, Permissions.SEND_NOTIFICATIONS);
+        sendBroadcast(broadcastIntent, Permissions.SEND_NOTIFICATIONS);
+    }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void sendNotif(BoardNews receiveNews) {
+        Intent intent = new Intent(this, BoardRiderFragment.class);
+        intent.putExtra(PARAM_RECEIVE_NEWS,receiveNews);
+        intent.putExtra(PARAM_NEWS_FLAG,true);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification notification  = new Notification.Builder(this)
+                .setContentTitle(receiveNews.getTitle())
+                .setContentText(receiveNews.getStringDate())
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setLargeIcon(getNewsImage(receiveNews.getImageUrl()))
+                .setContentIntent(pIntent)
+                .setAutoCancel(true).build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+    private Bitmap getNewsImage(String url){
+        Bitmap contactPic = null;
+
+        final String getOnlinePic = url;
+
+        try {
+            contactPic= Picasso.with(this).load(getOnlinePic)
+                    .resize(100, 100)
+                    .placeholder(R.drawable.image_polyfill)
+                    .error(R.drawable.image_polyfill)
+                    .get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  contactPic;
     }
 }
---
+++ app/src/main/java/goodline/info/notification/Permissions.java
@@ -0,0 +1,8 @@
+package goodline.info.notification;
+
+/**
+ * Created by Крабов on 17.05.2015.
+ */
+public class Permissions {
+    public static final String SEND_NOTIFICATIONS = "goodline.info.notification.Permissions.SEND_NOTIFICATIONS";
+}
