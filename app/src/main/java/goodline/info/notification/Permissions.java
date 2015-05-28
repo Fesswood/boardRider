757c74ff5f40f1d5dd4d87d98407417b06c07212
Fesswood <Fesswood@kemsu.ru>
2015-05-28 08:23:50 +0700

    refactoring commit

--- app/src/main/java/goodline/info/notification/Permissions.java
+++
@@ -1,8 +0,0 @@
-package goodline.info.notification;
-
-/**
- * Created by Крабов on 17.05.2015.
- */
-public class Permissions {
-    public static final String SEND_NOTIFICATIONS = "goodline.info.notification.Permissions.SEND_NOTIFICATIONS";
-}
